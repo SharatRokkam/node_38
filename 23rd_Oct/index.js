67c80d2bf075e22c230c266fd2037b7127400744
SharatRokkam <rokkamsharath1@gmail.com>
2024-10-24 10:48:52 +0530

    middleware and api

---
+++ 23rd_Oct/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const morgan = require("morgan");
+//functionality
+const app = express();
+const port = 3000;
+// const fs = require("fs");
+
+// const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+// const product = data.products;
+
+//Third party middleware
+
+app.use(morgan("default "));
+
+//application level middleware
+// app.use((req, res, next) => {
+//   console.log(req.ip, req.hostname, req.method, req.get("User-Agent"));
+// });
+
+//built-in middleware
+//bodyparser
+// app.use(express.json());
+app.use(express.static("public"));
+// app.use(express.urlencoded())
+
+// const auth = (req, res, next) => {
+//   console.log(req.query);
+
+//   if (req.body.password == "123") {
+//     next();
+//   } else {
+//     res.sendStatus(401);
+//   }
+// };
+
+// API - Endpoints
+app.get("/", function (req, res) {
+  // response.sendFile("./index.html");
+  res.json({ type: "get" });
+});
+
+app.post("/", (req, res) => {
+  res.json({ type: "post" });
+});
+
+app.put("/", (req, res) => {
+  res.json({ type: "put" });
+});
+
+app.patch("/", (req, res) => {
+  res.json({ type: "patch" });
+});
+
+app.delete("/", (req, res) => {
+  res.json({ type: "delete" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
